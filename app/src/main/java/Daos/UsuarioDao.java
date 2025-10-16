553899079466fbf235faa0a8f2ac2a894fad9a62
lourdesgv19 <guerrierivluli@gmail.com>
2025-10-16 20:06:10 -0300

    Tabla barberoServicios

--- app/src/main/java/Daos/UsuarioDao.java
+++ app/src/main/java/Daos/UsuarioDao.java
@@ -24,7 +24,7 @@ public interface UsuarioDao {
     Usuario findByEmail(String email);
 
     @Query("SELECT * FROM usuarios WHERE rol = :rol")
-    List<Usuario> obtenerPorRol(String rol);
+    List<Usuario> findByRole(String rol);
 
     @Query("SELECT * FROM usuarios")
     LiveData<List<Usuario>> getAllUsersAsLiveData();
